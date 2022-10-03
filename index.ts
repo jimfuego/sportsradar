b1d50610cfc410ed037e74d3b27983446c8b4c93
jimmyb <jimmyb89@yahoo.com>
2022-10-03 12:14:28 -0500

    code cleanup

--- index.ts
+++ index.ts
@@ -3,11 +3,13 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
+
 app.get('/', (req, res, next) => {
   res.status(200).json({
-    message: 'Running Node with Express and Typescript',
+    message: 'Hello, there!',
   });
 });
+
 const server = app.listen(PORT, () => {
   console.log(`Server running on ${PORT}.`);
 });
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,22 +1,12 @@
-import { doesNotMatch } from 'assert';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import { app, server } from '../index';
 chai.use(chaiHttp);
 
 describe('Our lovely server', () => {
-  // Called once before any of the tests in this block begin.
-  // before(function(done) {
-  //   app.listen(function(err: any) {
-  //     if (err) { return done(err); }
-  //     done();
-  //   })
-  // });
-
   it('Should always pass', function () {
     expect(1).to.equal(1);
   });
-
   it('GET /', function (done) {
     chai
       .request(app)
