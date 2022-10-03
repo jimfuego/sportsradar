011bc9133d190cfa54d1285ea54896707a477d7b
jimmyb <jimmyb89@yahoo.com>
2022-10-03 11:29:14 -0500

    add db connection

--- utils/db.ts
+++ utils/db.ts
@@ -1,20 +1,20 @@
-import postgres from 'postgres'
-interface IOptions{
-  host: string,
-  port: number,
-  database: string,
-  username: string,
-  password:string,
+import postgres from 'postgres';
+interface IOptions {
+  host: string;
+  port: number;
+  database: string;
+  username: string;
+  password: string;
 }
 
-let options:IOptions = {
+const options: IOptions = {
   host: process.env.PG_HOST as string,
   port: process.env.PG_PORT as unknown as number,
   database: process.env.PG_DATABASE as string,
   username: process.env.PG_USERNAME as string,
-  password:process.env.PG_PASSWORD as string,
-}
+  password: process.env.PG_PASSWORD as string,
+};
 
-const sql = postgres(options) // will use psql environment variables
+const sql = postgres(options); // will use psql environment variables
 
-export default sql
+export default sql;
